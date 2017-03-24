198afaf75fffc4c70425b64d42190ec6b7b2b1e5
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-03-25 04:27:06 +0800

    closure test

--- Chapter7/closure.js
+++ Chapter7/closure.js
@@ -1,3 +1,32 @@
-(function (){
-    alert("Test if run!");
-})();
+//(function (){
+//    alert("Test if run!");
+//})();
+
+function createComparisonFunction(propertyName){
+    return function(object1, object2){
+        var value1 = object1[propertyName];
+        var value2 = object2[propertyName];
+        
+        if(value1 < value2){
+            return -1;
+        }else if(value1 > value2){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+
+var object1 = {
+    "name":"jay",
+    "age":33
+}
+
+var object2 = {
+    "name":"tab",
+    "age":28
+}
+
+var func = createComparisonFunction("name");
+var result = func(object1, object2);
+alert(result);
