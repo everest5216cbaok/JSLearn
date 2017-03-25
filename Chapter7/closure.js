eccee991a772f9d9c78d1c3dd165ef3355fcae68
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-03-25 13:30:33 +0800

    The closure always gets the last value of any variable from the containing function.

--- Chapter7/closure.js
+++ Chapter7/closure.js
@@ -2,31 +2,49 @@
 //    alert("Test if run!");
 //})();
 
-function createComparisonFunction(propertyName){
-    return function(object1, object2){
-        var value1 = object1[propertyName];
-        var value2 = object2[propertyName];
-        
-        if(value1 < value2){
-            return -1;
-        }else if(value1 > value2){
-            return 1;
-        }else{
-            return 0;
+//function createComparisonFunction(propertyName){
+//    return function(object1, object2){
+//        var value1 = object1[propertyName];
+//        var value2 = object2[propertyName];
+//        
+//        if(value1 < value2){
+//            return -1;
+//        }else if(value1 > value2){
+//            return 1;
+//        }else{
+//            return 0;
+//        }
+//    }
+//}
+//
+//var object1 = {
+//    "name":"jay",
+//    "age":33
+//}
+//
+//var object2 = {
+//    "name":"tab",
+//    "age":28
+//}
+//
+//var func = createComparisonFunction("name");
+//var result = func(object1, object2);
+//alert(result);
+
+function createFunctions(){
+    var result = new Array();
+    
+    for(var i = 0; i < 10; i++){
+        result[i] = function(){
+            return i;
         }
     }
+    
+    return result;
 }
 
-var object1 = {
-    "name":"jay",
-    "age":33
+var result = createFunctions();
+for(var i=5; i<result.length; ++i){
+    console.log(result[i]());
+    //alert(result[i]());
 }
-
-var object2 = {
-    "name":"tab",
-    "age":28
-}
-
-var func = createComparisonFunction("name");
-var result = func(object1, object2);
-alert(result);
