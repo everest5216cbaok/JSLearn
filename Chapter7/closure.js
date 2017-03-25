d4a8d21321f3f6b2de1e8842ba04f6a1e9845cbe
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-03-25 13:33:29 +0800

    force the closures to act appropriately by creating another anonymous function

--- Chapter7/closure.js
+++ Chapter7/closure.js
@@ -35,9 +35,11 @@ function createFunctions(){
     var result = new Array();
     
     for(var i = 0; i < 10; i++){
-        result[i] = function(){
-            return i;
-        }
+        result[i] = (function(num){
+            return function(){
+                return num;
+            }
+        })(i);
     }
     
     return result;
