3d8460a84378797422c6499aea12ff0cbb2d5db9
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-03-27 13:59:33 +0800

    inherit test

---
+++ Chapter6/PrototypeChaining.js
@@ -0,0 +1,25 @@
+function SuperType(){
+    this.property = true;
+}
+
+SuperType.prototype.getSuperValue = function(){
+    return this.property;
+}
+
+function SubType(){
+    this.subproperty = false;
+}
+
+//inherit from SuperType
+SubType.prototype = new SuperType();
+
+SubType.prototype.getSubValue = function(){
+    return this.subproperty;
+}
+
+var instance = new SubType();
+alert(instance.getSuperValue());
+
+var property = false;
+SuperType();
+alert(property);
