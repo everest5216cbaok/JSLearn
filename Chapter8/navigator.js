61972f5d662d895642174852155152612b4e47f5
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-03-30 22:30:11 +0800

    navigator property and method

---
+++ Chapter8/navigator.js
@@ -0,0 +1,5 @@
+alert(window.navigator.appCodeName);
+alert(window.navigator.appVersion);
+alert(window.navigator.cookieEnabled);
+alert(window.navigator.javaEnabled());
+alert(window.navigator.product);
