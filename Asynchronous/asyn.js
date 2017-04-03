1a0967f743e321e2e3efe4dc9f19558271654314
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-04-03 14:47:26 +0800

    回调函数异步，这个不太理解！

--- Asynchronous/asyn.js
+++ Asynchronous/asyn.js
@@ -1,10 +1,20 @@
-function f1(){
-    alert("run f1");
+//function f1(){
+//    alert("run f1");
+//}
+
+function f1(callback){
+    setTimeout(function(){
+        alert("run f1");
+        
+        callback();
+    }, 1000);
 }
 
 function f2(){
     alert("run f2");
 }
 
-f1();
-f2();
+//f1();
+//f2();
+
+f1(f2);
