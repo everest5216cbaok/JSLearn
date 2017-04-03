70e2b8f47f8a4a02744c45821980f662a67c6fac
everest5216cbaok <everest5216cbaok@hotmail.com>
2017-04-03 14:41:15 +0800

    test if has plugin

--- Chapter8/navigator.js
+++ Chapter8/navigator.js
@@ -1,5 +1,19 @@
-alert(window.navigator.appCodeName);
-alert(window.navigator.appVersion);
-alert(window.navigator.cookieEnabled);
-alert(window.navigator.javaEnabled());
-alert(window.navigator.product);
+//alert(window.navigator.appCodeName);
+//alert(window.navigator.appVersion);
+//alert(window.navigator.cookieEnabled);
+//alert(window.navigator.javaEnabled());
+//alert(window.navigator.product);
+
+function hasPlugin(name){
+    name = name.toLowerCase();
+    for(var i = 0; i < navigator.plugins.length; ++i){
+        if(navigator.plugins[i].name.toLowerCase().indexOf(name) > -1){
+            return true;
+        }
+    }
+    return false;
+}
+
+alert(hasPlugin("Flash"));
+
+alert(hasPlugin("QuickTime"));
